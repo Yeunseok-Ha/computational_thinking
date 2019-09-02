909181bb97bb35d24b640ed2c430aa1f46761763
yeun <kus01113@gmail.com>
2019-09-03 00:43:45 +0900

    pa1

--- pa1_3.c
+++ pa1_3.c
@@ -7,38 +7,40 @@ struct building{
 	int x;
 };
 
-void merge(struct building* list, int left, int mid, int right, struct building* sorted);
-void merge_sort(struct building* list, int left, int right, struct building* sorted);
+//struct building b[10000000];
+struct building sorted[10000000];
+void merge(struct building list[], int left, int mid, int right);
+void merge_sort(struct building list[], int left, int right);
 int main (void)
 {
 	int k, i, j, n, to;
 	int sum_g = 0;
 	int max_g = 0;
 	int s = 0;
-	
-	struct building* b;
-	//get numbers of buildings and waling distance
+	struct building b[1000000];	
+//	struct building* b;
 	scanf("%d %d", &n, &k);
 
-	b = malloc(sizeof(struct building)*n);
+//	b = malloc(sizeof(struct building)*n);
 	
-	//get location of each buildings and its included people
 	for(i = 0; i < n; i++){
 		scanf("%d %d", &b[i].g, &b[i].x);
 	}
-	struct building* sorted = malloc(sizeof(struct building)*n);
+//	struct building* sorted = malloc(sizeof(struct building)*n);
 	
-	merge_sort(b, 0, n-1, sorted);
-//	for(i = 0; i < n; i++)
-//		printf("x : %d, g : %d\n", b[i].x, b[i].g);
+	merge_sort(b, 0, n-1);
+	for(i = 0; i < n; i++)
+		printf("x : %d g : %d\n", b[i].x, b[i].g);
+	
 	for(i = 0; i < n; i++){
 		s = b[i].x;
 		//from = i;
 		to = i;
-		while(s >= b[i].x && s <= b[i].x + (2*k) && s <= b[n-1].x ){
+		while(s <= b[i].x+(2*k) && to < n){
 			sum_g += b[to].g;
-			++to;
+			to++;
 			s = b[to].x;
+			//printf("a\n");
 		}
 		
 		if(sum_g > max_g){
@@ -50,11 +52,11 @@ int main (void)
 
 	printf("%d", max_g);
 	
-	free(b);
+//	free(b);
 	return 0;
 }
 
-void merge(struct building* list, int left, int mid, int right, struct building* sorted)
+void merge(struct building list[], int left, int mid, int right)
 {
 	int i, j, k, l;
 	i = left;
@@ -82,12 +84,12 @@ void merge(struct building* list, int left, int mid, int right, struct building*
 	}
 }
 
-void merge_sort(struct building* list, int left, int right, struct building* sorted){
+void merge_sort(struct building list[], int left, int right){
         int mid;
         if(left<right){
 		mid = (left+right)/2;
-		merge_sort(list, left, mid, sorted);
-		merge_sort(list, mid+1, right, sorted);
-		merge(list, left, mid, right, sorted);
+		merge_sort(list, left, mid);
+		merge_sort(list, mid+1, right);
+		merge(list, left, mid, right);
 	}
 }
