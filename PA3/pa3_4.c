d3c511f11a10fa4988ba09d4acf5a50230ab891c
yeun <kus01113@gmail.com>
2019-09-16 16:45:44 +0900

    pa3

--- PA3/pa3_4.c
+++ PA3/pa3_4.c
@@ -4,9 +4,6 @@
 
 #define INF 0x7fffffff
 #define SIZE 40000
-//#define initial 0
-//#define waiting 1
-//#define visited 2
 
 struct queue {
 	int items[SIZE];
@@ -36,14 +33,12 @@ struct Graph
 };
 struct Graph* createGraph(int vertices);
 void addEdge(struct Graph* graph, int src, int dest);
-void printGraph(struct Graph* graph);
 void bfs(struct Graph* graph, int startVertex);
 int main(void)
 {
 	int min_fuel, sum;
 	int i, j, p, q, r, N, M;
 	int from, to;
-	//struct Graph* map = (struct Graph*) malloc(sizeof(struct Graph));
 
 	scanf("%d %d %d %d %d",&p, &q, &r, &N, &M);
 	
@@ -54,9 +49,10 @@ int main(void)
 		scanf("%d %d", &from, &to);
 		addEdge(map, from-1, to-1);
 	}
+
+	bfs(map, N-1);
 	bfs(map, 0);
 	bfs(map, 1);
-	bfs(map, N-1);
 	min_fuel = (map->reachable_a[N-1]*p) + (map->reachable_b[N-1]*q);
 	
 /*	for(i = 2; i < N; i++){
@@ -83,7 +79,7 @@ int main(void)
 			}
 			if(map->fromDst[j] != (map->reachable_a[N-1] - map->reachable_a[j]) 
 					|| map->fromDst[j] != (map->reachable_b[N-1] - map->reachable_b[j])
-					&& temp[j] != 1){
+					&& map->adj_count[j] > 2){
 				sum = (map->reachable_a[j]*p) + (map->reachable_b[j]*q) +(map->fromDst[j]*r);
 				if(sum < min_fuel)
 					min_fuel = sum;
@@ -101,7 +97,7 @@ int main(void)
 void bfs(struct Graph* graph, int startVertex) {
 	int *storeDist = malloc(sizeof(int)*(graph->numVertices));
 	struct queue* q = createQueue();
-//	int returnDist;
+	int a = 0;
 
 	for(int i = 0; i < graph->numVertices; i++){
 		graph->visited[i]=0;
@@ -109,16 +105,23 @@ void bfs(struct Graph* graph, int startVertex) {
 	}
 	graph->visited[startVertex] = 1;
 	enqueue(q, startVertex);
+	
+	graph->reachable_a[0] = 0;
+	graph->reachable_b[1] = 0;
 
 	while(!isEmpty(q)){
-//		printQueue(q);
 		int currentVertex = dequeue(q);
-//		printf("Visited %d\n", currentVertex);
 		
 		struct node* temp = graph->adjLists[currentVertex];
 						           
 		while(temp) {
 			int adjVertex = temp->vertex;
+//			if((startVertex == 0) && (graph->fromDst[0] - graph->fromDst[currentVertex] == graph->reachable_a[currentVertex])){
+//				break;
+//			}else if((startVertex == 1) && (graph->fromDst[1] - graph->fromDst[currentVertex] == graph->reachable_b[currentVertex])){
+//				break;
+//			}
+
 			if(graph->visited[adjVertex] == 0){
 				storeDist[adjVertex] = storeDist[currentVertex]+1;
 				if(startVertex == 0){
@@ -128,20 +131,23 @@ void bfs(struct Graph* graph, int startVertex) {
 				else if(startVertex == 1){
 					graph->reachable_b[adjVertex] = storeDist[adjVertex];
 					graph->storePare_b[adjVertex] = currentVertex;
-				}else
+				}else{
 					graph->fromDst[adjVertex] = storeDist[adjVertex];
+//					graph->reachable_a[adjVertex] = storeDist[adjVertex];
+//					graph->reachable_b[adjVertex] = storeDist[adjVertex];
+				}
 				graph->visited[adjVertex] = 1;
 				enqueue(q, adjVertex);
 			}
 				temp = temp->next;
 		}
 	}
-//	printf("from %d : \n",startVertex);
-//	for(int i = 0; i < finishVertex+1; i++)
-//		printf("%d \t", graph->storePare_a[i]);
-//	printf("\n");
-//	for(int i = 0; i < finishVertex+1; i++)
-//		printf("%d \t", graph->storePare_b[i]);
+	printf("from %d : \n",startVertex);
+	for(int i = 0; i < graph->numVertices; i++)
+		printf("%d \t", storeDist[i]);
+	printf("\n");
+//	for(int i = 0; i < graph->numVertices; i++)
+//		printf("%d \t", graph->reachable_b[i]);
 //	printf("\n");
 	free(q);
 	free(storeDist);
@@ -174,11 +180,11 @@ struct Graph* createGraph(int vertices)
 		graph->adjLists[i] = NULL;
 		graph->visited[i] = 0;
 		graph->adj_count[i] = 0;
-		graph->reachable_a[i] = -1;
-		graph->reachable_b[i] = -1;
+//		graph->reachable_a[i] = -1;
+//		graph->reachable_b[i] = -1;
 		graph->storePare_a[i] = -1;
 		graph->storePare_b[i] = -1;
-		graph->fromDst[i] = -1;
+//		graph->fromDst[i] = -1;
 	}
 	
 	return graph;
@@ -209,39 +215,17 @@ int isEmpty(struct queue* q) {
 	        return 0;
 }
 void enqueue(struct queue* q, int value){
-//        if(q->rear == SIZE-1)
-//	        printf("\nQueue is Full!!");
-//	else {
-	        if(q->front == -1)
-		        q->front = 0;
-                q->rear++;
-                q->items[q->rear] = value;
-//	}
+	if(q->front == -1)
+		q->front = 0;
+        q->rear++;
+        q->items[q->rear] = value;
 }
 int dequeue(struct queue* q){
         int item;
-//        if(isEmpty(q)){
-//                printf("Queue is empty");
-//        	item = -1;
-//	}
-//	else{
-	        item = q->items[q->front];
-	        q->front++;
-		if(q->front > q->rear){
-//		        printf("Resetting queue");
-	                q->front = q->rear = -1;
-		}
-//	}
+	item = q->items[q->front];
+	q->front++;
+	if(q->front > q->rear){
+	        q->front = q->rear = -1;
+	}
         return item;
-}
-void printQueue(struct queue *q) {
-        int i = q->front;
-        if(isEmpty(q)) {
-	        printf("Queue is empty");
-	} else {
-	        printf("\nQueue contains \n");
-	        for(i = q->front; i < q->rear + 1; i++) {
-		        printf("%d ", q->items[i]);
-	        }
-	}    
 }
